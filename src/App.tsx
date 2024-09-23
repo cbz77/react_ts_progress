9231f08068dc3ac10a9fdc064fd3a375f16f40f3
gerek <gerek@ebm.cz>
2024-09-23 09:49:28 +0200

    components - progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,18 @@
 import './App.css';
-import { useState } from 'react';
-import TaskCheckbox from './TaskCheckbox';
+import ProgressTest from './components/progress/Progress';
 
 function App() {
-
-  const [progressBarVal, setProgressBarVal] = useState(0)
-  const pocetCheckboxu: number = 4;
-  const progressPrirustek: number = 100 / pocetCheckboxu;
-
-  const handleChange = (e: any) => {
-
-    if(e.currentTarget.checked == true){
-      setProgressBarVal(progressBarVal + progressPrirustek);
-    }else{
-      setProgressBarVal(progressBarVal - progressPrirustek);
-    }
-
-  }
-
 
   return (
     <div className="container mx-auto shopping_cart_app m-5 p-5">
       
       <header className='text-center'>
-        <h1 className='text-4xl text-bold'>Progress</h1>
+        <h1 className='text-4xl text-bold'>Test Typescript</h1>
       </header>
 
+      <ProgressTest></ProgressTest>
 
-      <progress className="progress mt-5" value={progressBarVal} max={100}></progress>
-
-      <div className='container max-w-28 mx-auto'>
-
-        
-        <TaskCheckbox textCheckboxu="Checkbox1" idCheckboxu="checkbox_1" onChange={handleChange}></TaskCheckbox>
-        <TaskCheckbox textCheckboxu="Checkbox2" idCheckboxu="checkbox_2" onChange={handleChange}></TaskCheckbox>
-        <TaskCheckbox textCheckboxu="Checkbox3" idCheckboxu="checkbox_3" onChange={handleChange}></TaskCheckbox>
-        <TaskCheckbox textCheckboxu="Checkbox4" idCheckboxu="checkbox_4" onChange={handleChange}></TaskCheckbox>
-        
-
-      </div>
-     
+      <div className='divider'></div>
 
     </div>
   );
---
+++ src/components/progress/Progress.tsx
@@ -0,0 +1,39 @@
+import TaskCheckbox from "./TaskCheckbox"
+import { useState } from "react";
+
+const ProgressTest: React.FC = () => {
+
+    const [progressBarVal, setProgressBarVal] = useState(0)
+    const pocetCheckboxu: number = 4;
+    const progressPrirustek: number = 100 / pocetCheckboxu;
+
+    const handleChange = (e: any) => {
+
+        if(e.currentTarget.checked == true){
+        setProgressBarVal(progressBarVal + progressPrirustek);
+        }else{
+        setProgressBarVal(progressBarVal - progressPrirustek);
+        }
+
+    }
+
+    return(
+        <div className="progress_test">
+
+            <h2>Progress bar:</h2>
+
+            <progress className="progress mt-5" value={progressBarVal} max={100}></progress>
+
+            <div className='container max-w-28 mx-auto'>
+            
+            <TaskCheckbox textCheckboxu="Checkbox1" idCheckboxu="checkbox_1" onChange={handleChange}></TaskCheckbox>
+            <TaskCheckbox textCheckboxu="Checkbox2" idCheckboxu="checkbox_2" onChange={handleChange}></TaskCheckbox>
+            <TaskCheckbox textCheckboxu="Checkbox3" idCheckboxu="checkbox_3" onChange={handleChange}></TaskCheckbox>
+            <TaskCheckbox textCheckboxu="Checkbox4" idCheckboxu="checkbox_4" onChange={handleChange}></TaskCheckbox>
+
+            </div>
+        </div>
+    )
+}
+
+export default ProgressTest
