a0130e56a1d36afd0550bc27a2697cce31c97c78
gerek <gerek@ebm.cz>
2024-09-23 12:13:26 +0200

    collapsed items

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,27 @@ function App() {
   return (
     <div className="container mx-auto shopping_cart_app m-5 p-5">
       
-      <header className='text-center'>
+      <header className='text-center mb-4'>
         <h1 className='text-4xl text-bold'>Test Typescript</h1>
       </header>
 
-      <ProgressTest></ProgressTest>
+      <div className='collapse bg-base-200'>
+        <input type='checkbox'></input>
+        <div className='collapse-title text-xl font-medium'>Progress</div>
+        <div className='collapse-content'>
+          <ProgressTest></ProgressTest>
+        </div>
+      </div>
 
       <div className='divider'></div>
 
-      <PaintCanvas></PaintCanvas>
+      <div className='collapse bg-base-200'>
+        <input type='checkbox'></input>
+        <div className='collapse-title text-xl font-medium'>Paint</div>
+        <div className='collapse-content'>
+          <PaintCanvas></PaintCanvas>
+        </div>
+      </div>
 
     </div>
   );
